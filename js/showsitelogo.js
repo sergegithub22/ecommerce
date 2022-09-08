548938e2b90bcb4cb3bc55c2a1ccabf9b179c027
Sergei <112075321+sergegithub22@users.noreply.github.com>
2022-09-08 14:10:14 +0300

    080922
    1410

--- js/showsitelogo.js
+++ js/showsitelogo.js
@@ -4,6 +4,7 @@ function showHideSiteLogo() {
   logo.classList.remove("visually-hidden");
   link[0].textContent = "Hide Site Logo";
   window.alert("Now u will see the logo of this site template. For hide it - click, please, on 'Hide Site Logo' in 'My Account' menu");
+    
 }  
   else{
     logo.classList.add("visually-hidden");
@@ -15,7 +16,8 @@ function showHideSiteLogo() {
 var link = document.querySelectorAll(".show_site_logo");
 var logo = document.querySelector(".logo");
 
+
 for (var i = link.length - 1; i >= 0; i--) {
 	link[i].addEventListener("click", showHideSiteLogo);
 }
-console.log([logo]);
+console.log(logo.style);
